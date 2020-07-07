865af3bf3cefe9107844fa3d6475d5a8358bbe19
Conor Lawless <cnr.lwlss@gmail.com>
2020-07-07 18:54:31 +0100

    Add Alice in Wonderland and assignment about parsing text

---
+++ code/parseText.jl
@@ -0,0 +1,19 @@
+fname = "AliceInWonderland.txt"
+fstring = read(fname,String)
+farr = split(fstring)
+
+# How many words are there in Alice in Wonderland?
+
+# What is the last word?
+
+# What is the last word before "THE END"?
+
+# This is an example of how you might count the number of times the word "Alice appears":
+count(i->(lowercase(i)=="alice"),farr)
+
+# Write a function that will count the number of times any word appears
+
+# Make the function work even if I passed it an argument with capital letters (e.g. "Alice")?
+
+# Make an array which tells me how many times the following words appear:
+interesting_words = ["alice","mushroom", "caterpillar", "head", "cheshire"]
