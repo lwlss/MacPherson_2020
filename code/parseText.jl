0dc2421f8fb667b3c1ccd78c52b3aa179a68ee9c
Conor Lawless <cnr.lwlss@gmail.com>
2020-07-07 23:22:04 +0100

    Turtle demo and use .gitignore to keep .png files out of repo

--- code/parseText.jl
+++ code/parseText.jl
@@ -8,12 +8,12 @@ farr = split(fstring)
 
 # What is the last word before "THE END"?
 
-# This is an example of how you might count the number of times the word "Alice appears":
+# This is an example of how you might count the number of times the word "alice" appears:
 count(i->(lowercase(i)=="alice"),farr)
 
 # Write a function that will count the number of times any word appears
 
 # Make the function work even if I passed it an argument with capital letters (e.g. "Alice")?
 
-# Make an array which tells me how many times the following words appear:
+# Make a collection which tells me how many times the following words appear:
 interesting_words = ["alice","mushroom", "caterpillar", "head", "cheshire"]
---
+++ code/turtleDemo.jl
@@ -0,0 +1,19 @@
+using Luxor, Colors
+Drawing(6000,4000,"turtle.png")
+origin()
+background("midnightblue")
+
+turtle = Turtle()
+Pencolor(turtle, "cyan")
+Penwidth(turtle, 1.5)
+n = 5
+for i in 1:4000
+    global n
+    Forward(turtle, n)
+    Turn(turtle, 89.5)
+    HueShift(turtle)
+    n += 0.75
+end
+fontsize(20)
+#Message(turtle, "finished")
+finish()
