2531f0f20dfe1b7306bc9abc92f986d1ab22b17d
patrick-mac <66121326+patrick-mac@users.noreply.github.com>
2020-07-09 22:01:36 +0100

    Logistic Map Function

--- code/logisticmap.jl
+++ code/logisticmap.jl
@@ -1,3 +1,19 @@
-function logic(r,x)
-    y=(r*x)(1-x)
+function logistic(r,x)
+    r*x*(1.0-x)
 end
+
+
+x0 = 0.2
+r = 3.7
+results = [(x0,logistic(r,x0))];
+# We've already seen arrays: [1,2,3,4]
+# Here I'm using tuples: (1,2,3,4)
+# Results is an array filled with tuples!
+N = 1000; # or any big number
+for i in 1:(N-1)
+  xold = results[end][2]
+  xnew = logistic(r,xold)
+  append!(results,[(xold,xnew)])
+end
+
+print(results)
--- code/turtleDemo2.jl
+++ code/turtleDemo2.jl
@@ -21,4 +21,49 @@ finish()
 
 using Pkg
 Pkg.add("Luxor")
-Pkg.add("Color")
+Pkg.add("Colo
+
+
+
+using Luxor, Colors
+Drawing(6000,4000,"turtlethree.png")
+origin()
+background("orange")
+
+turtle = Turtle()
+Pencolor(turtle, "blue")
+Penwidth(turtle, 0.5)
+n = 5
+for i in 1:8000
+    global n
+    Forward(turtle, n)
+    Turn(turtle, 95)
+    HueShift(turtle)
+    n += 0.85
+end
+fontsize(20)
+#Message(turtle, "finished")
+finish()
+
+
+using Luxor, Colors
+function draw(w,t)
+    Drawing(6000,4000,"turtlethree.png")
+    origin()
+    background("orange")
+
+    turtle = Turtle()
+    Pencolor(turtle, "blue")
+    Penwidth(turtle, w)
+    n = 5
+    for i in 1:8000
+        global n
+        Forward(turtle, n)
+        Turn(turtle, t)
+        HueShift(turtle)
+        n += 0.85
+    end
+    fontsize(20)
+    #Message(turtle, "finished")
+    finish()
+end
