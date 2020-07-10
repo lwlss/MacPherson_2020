80578bc738c29a8d59b2e17c80eab79255331735
Conor Lawless <cnr.lwlss@gmail.com>
2020-07-10 23:38:16 +0100

    Clean up and get function working

--- code/logisticmap.jl
+++ code/logisticmap.jl
@@ -2,55 +2,15 @@ function logistic(r,x)
     r*x*(1.0-x)
 end
 
-x0 = 0.2
-r = 3.7
-results = [(x0,logistic(r,x0))];
-# We've already seen arrays: [1,2,3,4]
-# Here I'm using tuples: (1,2,3,4)
-# Results is an array filled with tuples!
-N = 100; # or any big number
-for i in 1:(N-1)
-  xold = results[end][2]
-  xnew = logistic(r,xold)
-  append!(results,[(xold,xnew)])
-end
-
-print(results)
-
-function logisticmap(x0,r)
+function logisticmap(x0,r,N)
     results = [(x0,logistic(r,x0))];
-    # We've already seen arrays: [1,2,3,4]
-    # Here I'm using tuples: (1,2,3,4)
-    # Results is an array filled with tuples!
-    N = 100; # or any big number
     for i in 1:(N-1)
       xold = results[end][2]
       xnew = logistic(r,xold)
       append!(results,[(xold,xnew)])
       print(results)
   end
+  results
 end
 
-
-
-using PlotlyJS
-using Distributions
-
-labs=["1","2","3","4","5"]
-N=100;
-x=1:N;
-y = for i in 1:(N-1)
-  xold = results[end][2]
-  xnew = logistic(r,xold)
-  append!(results,[(xold,xnew)])
-end
-
-layout = layout(
-    title = "Logistical Map"
-    xaxis_title="Iterations (n)"
-    yaxis_title="Value of x (xn+1)"
-    font_family = "Arial",
-    font_size = 12
-)
-
-plot(randomwalks,layout)
+print(logisticmap(0.2,3.7,10))
