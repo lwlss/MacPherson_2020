4beba4783b58d54a4236f64d42d53c58236f6f1c
Conor Lawless <cnr.lwlss@gmail.com>
2020-07-22 16:24:41 +0100

    Edits during meeting

--- code/week_three-differential_equations/exponential_logistic_models.jl
+++ code/week_three-differential_equations/exponential_logistic_models.jl
@@ -1,4 +1,5 @@
 using DifferentialEquations
+using Plots; gr()
 
 f(u,p,t) = 1.01*u
 u0 = 1/2
@@ -7,23 +8,20 @@ prob = ODEProblem(f,u0,tspan)
 
 sol = solve(prob)
 
-using Plots; gr()
 plot(sol)
 
 plot(sol,linewidth=5,title="Solution to the linear ODE with a thick line",
-     xaxis="Time (t)",yaxis="u(t) (in μm)",label="Line")
+     xaxis="Time (d)",yaxis="Population size",label="Numerical solution")
 
 plot!(sol.t,t->0.5*exp(1.01t),lw=3,ls=:dash,label="True Solution")
 
 ############################################################################
 
-f(u,p,t) = 1.5*u*(1-(u/p))
-u0 = 1
-p = 3
-tspan = (0.0, 1.0)
-prob2 = ODEProblem(f, u0, p, tspan)
+f(u,p,t) = 1.5*u*(1-(u/3.0))
+u0 = 0.01
+tspan = (0.0, 10.0)
+prob2 = ODEProblem(f, u0, tspan)
 
 sol2 = solve(prob2)
 
-using Plots; gr()
-plot(sol)
+plot(sol2)
