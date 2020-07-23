6af2194374364d0ac674576e62b75f8311108f13
patrick-mac <66121326+patrick-mac@users.noreply.github.com>
2020-07-23 12:08:43 +0100

    added true line of logistic graph and moved legend

--- code/week_three-differential_equations/exponential_logistic_models.jl
+++ code/week_three-differential_equations/exponential_logistic_models.jl
@@ -3,8 +3,8 @@ using Plots; gr()
 
 f(u,p,t) = p[1]*u
 u0 = 0.5
-params = [3.0]
-
+#params = [3.0]
+p = [3.0]
 tspan = (0.0,1.0)
 prob = ODEProblem(f,u0,tspan,params)
 
@@ -21,11 +21,17 @@ plot!(sol.t,t->u0*exp(p[1]t),lw=3,ls=:dash,label="True Solution")
 
 f(u,p,t) = p[1]*u*(1-(u/p[2]))
 #f(u,p,t) = p[1]*u*(1-u)
-u0 = 0.5
-params = [5.0,1.0]
+u0 = 0.1
+#params = [5.0,1.0]
+p = [5.0,1.0]
 tspan = (0.0, 10.0)
-prob2 = ODEProblem(f, u0, tspan, params)
-
-sol2 = solve(prob2,reltol=1e-7,abstol=1e-7)
+#prob2 = ODEProblem(f, u0, tspan, params)
+prob2 = ODEProblem(f, u0, tspan, p)
+sol2 = solve(prob2,reltol=1e-8,abstol=1e-8)
 
 plot(sol2)
+
+plot(sol2,linewidth=5,title="Logistic ODE Graph",
+     xaxis="Time (s)",yaxis="Population size",label="Numerical Solution", legend=:bottomright)
+
+plot!(sol2.t,t->((p[2])*(u0*exp(p[1]t)))/(p[2]+(u0*(exp(p[1]t)-1))),lw=3,ls=:dash,label="True Solution")
