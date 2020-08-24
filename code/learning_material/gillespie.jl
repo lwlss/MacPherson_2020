9c1b4d0bfe5ee348c027befd0f35db076b3c5682
patrick-mac <66121326+patrick-mac@users.noreply.github.com>
2020-08-24 22:43:09 +0100

    updated graphs again

--- code/learning_material/gillespie.jl
+++ code/learning_material/gillespie.jl
@@ -99,14 +99,14 @@ sol = sim_gillespie(;b = 0.001875*365,d = 0.001875*365*0.86,bmut = 0.001875*365,
 nsims=150
 transparency = 0.6
 thick = 0.25
-solutions =[sim_gillespie(;b = 0.001875*365,d = 0.001875*365*0.86,bmut = 0.001875*365*1.1,dmut = 0.001875*365*0.86,m = (5e-7)*365, target = 100.0,vals0 = [30, 70],tmax = 100.0) for i in 1:nsims]
-panelA = plot(solutions[1].t,[solutions[1].u[i][1]+solutions[1].u[i][2] for i in 1:length(solutions[1].t)], labels="mtDNA copy number", color=RGBA(0,0,0,transparency), lw=thick)
+solutions =[sim_gillespie(;b = 0.001875*365,d = 0.001875*365*0.86,bmut = 0.001875*365*1.0,dmut = 0.001875*365*0.86,m = (5e-7)*365, target = 100.0,vals0 = [30, 70],tmax = 100.0) for i in 1:nsims]
+panelA = plot(solutions[1].t,[solutions[1].u[i][1]+solutions[1].u[i][2] for i in 1:length(solutions[1].t)], labels="", color=RGBA(0,0,0,transparency), lw=thick)
 for j in 2:nsims
-    plot!(solutions[j].t,[solutions[j].u[i][1]+solutions[j].u[i][2] for i in 1:length(solutions[j].t)], labels="", color=RGBA(0,0,0,transparency), lw=thick, legend=:bottomleft, xaxis="Time (Years)", yaxis="Population Size", title="High initial pathogenic variant load where b=bmut", title_location=:left)
+    plot!(solutions[j].t,[solutions[j].u[i][1]+solutions[j].u[i][2] for i in 1:length(solutions[j].t)], labels="", color=RGBA(0,0,0,transparency), lw=thick, legend=false, xaxis="Time (Years)", yaxis="mtDNA copy number", title="High initial pathogenic variant load where b=bmut", title_location=:left)
 end
-panelB = plot(solutions[1].t,[solutions[1].u[i][2]/(solutions[1].u[i][1]+solutions[1].u[i][2]) for i in 1:length(solutions[1].t)], labels="Pathogenic variant load", color=RGBA(1,0,0,transparency), lw=thick)
+panelB = plot(solutions[1].t,[solutions[1].u[i][2]/(solutions[1].u[i][1]+solutions[1].u[i][2]) for i in 1:length(solutions[1].t)], labels="", color=RGBA(1,0,0,transparency), lw=thick)
 for j in 2:nsims
-    plot!(solutions[j].t,[solutions[j].u[i][2]/(solutions[j].u[i][1]+solutions[j].u[i][2]) for i in 1:length(solutions[j].t)], labels="", color=RGBA(1,0,0,transparency), lw=thick, legend=:bottomright, xaxis="Time (Years)", yaxis="Pathogenic variant over mtDNA copy number")
+    plot!(solutions[j].t,[solutions[j].u[i][2]/(solutions[j].u[i][1]+solutions[j].u[i][2]) for i in 1:length(solutions[j].t)], labels="", color=RGBA(1,0,0,transparency), lw=thick, legend=false, xaxis="Time (Years)", yaxis="Pathogenic variant load")
 end
 
 plot(panelA,panelB,layout=(1,2))
@@ -116,8 +116,8 @@ nsims=1
 transparency = 1.0
 thick = 0.8
 solutions =[sim_gillespie(;b = 0.001875*365,d = 0.001875*365*0.86,bmut = 0.001875*365*1.1,dmut = 0.001875*365*0.86,m = (5e-7)*365, target = 100.0,vals0 = [30, 70],tmax = 100.0) for i in 1:nsims]
-panelA = plot(solutions[1].t,[solutions[1].u[i][1]+solutions[1].u[i][2] for i in 1:length(solutions[1].t)], labels="mtDNA copy number", color=RGBA(0,0,0,transparency), lw=thick, legend=:bottomleft,  xaxis="Time (Years)", yaxis="Population Size", title="High initial pathogenic variant load where b<bmut", title_location=:left)
-panelB = plot(solutions[1].t,[solutions[1].u[i][2]/(solutions[1].u[i][1]+solutions[1].u[i][2]) for i in 1:length(solutions[1].t)], labels="Pathogenic variant load", color=RGBA(1,0,0,transparency), lw=thick, legend=:bottomright, xaxis="Time (Years)", yaxis="Pathogenic variant over mtDNA copy number")
+panelA = plot(solutions[1].t,[solutions[1].u[i][1]+solutions[1].u[i][2] for i in 1:length(solutions[1].t)], labels="mtDNA copy number", color=RGBA(0,0,0,transparency), lw=thick, legend=false,  xaxis="Time (Years)", yaxis="Population Size", title="High initial pathogenic variant load where b<bmut", title_location=:left)
+panelB = plot(solutions[1].t,[solutions[1].u[i][2]/(solutions[1].u[i][1]+solutions[1].u[i][2]) for i in 1:length(solutions[1].t)], labels="Pathogenic variant load", color=RGBA(1,0,0,transparency), lw=thick, legend=false, xaxis="Time (Years)", yaxis="Pathogenic variant over mtDNA copy number")
 
 plot(panelA,panelB,layout=(1,2))
 
